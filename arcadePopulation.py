378c17f2c0c23d46bb4bec940e4d452983192bd6
BrunoCuevas <b.cuevas@alumnos.upm.es>
2017-08-08 20:08:40 +0200

    Running: -Support for more than 2 pathotypes -Coinfection matrix

--- arcadePopulation.py
+++ arcadePopulation.py
@@ -2,107 +2,237 @@
 import numpy as np
 
 class arcadePopulation:
-	fields = [
-		('rx', 'float32'),
-		('ry', 'float32'),
-		('x', 'float32'),
-		('y', 'b1'),
-		('d', 'uint64'),
-		('w', 'float32'),
-		('A', 'b1'),
-		('G', 'int8')
+	commonFields = [('index', 'uint32'), ('rx', 'float32'), ('ry', 'float32'), ('A', 'b1'), ('G', 'int8'), ('I', 'b1')]
 
-	]
-	dpi = 15
-	mortality = 30
-	def __init__(self, size_x, size_y, genotypes = 'P0', scattering='Regular'):
+	dpi = 5
+	mortality = 145
+	def __init__(self, size_x, size_y, parametersDict):
+
+		beta = list()
+		pathotypesList = sorted(parametersDict['pathotypes'])
+		print(pathotypesList)
+		self.__patho = pathotypesList
+		self.__loc   = dict()
+		i = 0
+		for item in pathotypesList:
+			beta.append(parametersDict['beta'][item])
+			self.__loc[item] = i
+			i += 1
+		self.__beta = np.array(beta)
+		self.__C    = parametersDict['C']
 		print("arcadePopulation instance defined")
 		print("setting a population of %d hosts" %(int(size_x*size_y)))
+
 		self.__sx = size_x
 		self.__sy = size_y
-		self.__P = self.setPopulationList(size_x, size_y)
-		xx_coords, yy_coords = self.__setGridValues(size_x, size_y, res=0.5, eliptical=1.0)
+		size = size_x * size_y
+
+		self.__P = self.setPopulationList(size_x, size_y, self.commonFields)
+		xx_coords, yy_coords = self.__setGridValues(size_x, size_y)
+
 		self.__P['rx'] = xx_coords
 		self.__P['ry'] = yy_coords
-		self.__P['A']  = np.ones(size_y*size_x)
+		self.__P['A'][:] = True
+
+		print("common attributes set")
+
+		self.__x = np.zeros((size, len(pathotypesList)))
+		self.__y = np.zeros((size, len(pathotypesList)))
+		self.__d = np.zeros((size, len(pathotypesList)))
+		self.__w = np.zeros((size, len(pathotypesList)))
+
 		print("population set")
 
-	def setPopulationList(self, size_x, size_y):
+	# STATIC METHODS
+	## setPopulationList
+	## setGridValues
+	## createAttributeList
+	@staticmethod
+	def setPopulationList(size_x, size_y, fields):
 		total_size = size_y * size_x
-		population = np.zeros(total_size, dtype=self.fields)
+		population = np.zeros(total_size, dtype=fields)
+		population['index'] = np.arange(size_x*size_y)
 		return population
 
 	@staticmethod
-	def __setGridValues(size_x, size_y, res = 1.0, eliptical = 1.0):
+	def __setGridValues(size_x, size_y):
 		x_coordinates = np.arange(size_x)
 		y_coordinates = np.arange(size_y)
 		xx, yy = np.meshgrid(x_coordinates,y_coordinates)
 		xx = xx.astype('float32')
 		yy = yy.astype('float32')
-		#xx *= res
-		#yy *= res
-		#yy *= eliptical
 		return xx.reshape(size_x*size_y), yy.reshape(size_x*size_y)
 
+	@staticmethod
+	def __createAttributeList(listAttributes, numpyType='float32'):
+		attributes = list()
+		for item in listAttributes:
+			attributes.append((item, numpyType))
+		return attributes
+
+	def setSeed(self, seedValue, patho):
+		"""
+		arcadeSpots3 - arcadePopulation - setSeed()
+		:param seedValue:
+		:param patho:
+		:return:
+
+		It sets the exposition value of a given position to 1.0,
+		 setting the beginning of an epidemic. It must be specified
+		 for each pathotype of the epidemic
+		"""
+		try:
+			pathoLoc = self.__loc[patho]
+		except KeyError:
+			raise KeyError("There is no {0} pathotype specified".format(patho))
+		self.__x[seedValue, pathoLoc] = 1.0
+
+	def getPathotypes(self):
+		"""
+		arcadeSpots3 - arcadePopulation - getPathotypes()
+		:return:
+
+		It returns a list with the pathototypes that are included
+		within the population.
+		"""
+		return self.__patho
+
 	def getCoordinates(self):
+		"""
+		arcadeSpots3 - arcadePopulation - getCoordinates()
+		:return: numpy array, numpy array
+
+		 It returns two arrays (x,y) with the coordinates of each host
+		"""
 		return self.__P['rx'], self.__P['ry']
 
 	def getAlive(self):
+		"""
+		arcadeSpots3 - arcadePopulation - getAlive()
+		:return: numpy array
+
+		It returns an array with the alive state of the hosts
+		"""
 		return self.__P['A']
 
 	def getExposition(self):
-		return self.__P['x']
+		"""
+		arcadeSpots3 - arcadePopulation - getExposition()
+		:return: numpy array
+
+		It returns an array with the exposition states of the hosts
+		"""
+		return self.__x
 
 	def getInfective(self):
-		return self.__P['y']
+		"""
+		arcadeSpots3 - arcadePopulation - getInfective()
+		:return: numpy array
+
+		It returns an array with the infective states of the hosts
+		"""
+		return self.__y
+	#
+	# In development
+	def getTranmission(self):
+
+		transmission = self.__y.dot(self.__C.T)
+		transmission[transmission < 0] = 0.0
+		return transmission
 
 	def getDays(self):
-		return self.__P['d']
+		"""
+		arcadeSpots3 - arcadePopulation - getDays()
+		:return: numpy array
 
-	def setSeed(self, seedValue):
-		self.__P['x'][seedValue] = 1.0
+		 It returns an array with the exposition states of the hosts
+		"""
+		return self.__d
 
 	def updateInfective(self):
-		days = np.copy(self.__P['d'])
-		self.__P['d'][self.__P['x'] >= 1.0] += 1
-		self.__P['y'][self.__P['d'] >= self.dpi] = True
-		self.__P['y'] = self.__P['A'] * self.__P['y']
-		postDays = np.copy(self.__P['d'])
-		if any(postDays < days):
-			raise RuntimeError("no way man!")
+		"""
+		arcadeSpots3 - arcadePopulation - getDays()
+		:return:
+
+		It updates the infective states depending on the days post infection,
+		the alive state, and the number of coinfections
+		"""
+		# This is necessary to avoid double or triple infections
+
+		controlInfective = np.sum(self.__y, axis=1)
+		self.__d += self.__x >= 1.0
+		self.__y +=  ((self.__d == self.dpi).T *(controlInfective < 2)).T
+		self.__y =  (self.__P['A'] * self.__y.T).T
+
 	def updateExposition(self, deltaX):
-		self.__P['x'] += deltaX*self.__P['A']
-		self.__P['x'][self.__P['x'] > 1.0] = 1.0
+		"""
+		arcadeSpots3 - arcadePopulation - updateExposition()
+		:param deltaX: numpy array
+		:return:
+
+		Once provided a vector that specifies the contacts between infective hosts and
+		other hosts, it updates the exposition value by multiplying the contact by
+		the rate of transmission of each pathotype.
+		"""
+		self.__x += (self.__P['A'] * (self.__beta * deltaX).T).T
+		self.__x[self.__x > 1.0] = 1.0
 
 	def updateAlive(self):
+		"""
+		arcadeSpots3 - arcadePopulation - updateAlive()
+		:return:
 
-		self.__P['A'][self.__P['d'] > self.mortality] = False
-		self.__P['w'][self.__P['d'] == self.mortality + 1] = 1000
-
+		Updates the primary inoculum, which gets increased when hosts die, and it modifies
+		the values of the alive state
+		"""
+		self.__w += (self.__d == self.mortality)*(self.__y == True)*1000
+		self.__P['A'][np.sum(self.__d >= self.mortality, axis=1) >= 1.0] = False
 
 	def updateInoculum(self):
-		self.__P['w'] *= (1 - 34e-3)
+		"""
+		arcadeSpots3 - arcadePopulation - updateAlive()
+		:return:
+
+		Updates the primary inoculum upon degradation
+		"""
+		self.__w *= (1 - 3e-3)
 
 	def primaryInfection(self):
-		primaryInfections = self.__P['A']*(np.random.rand(self.__sx*self.__sy) < (self.__P['w']/10000))
-		self.__P['x'] += primaryInfections
-		self.__P['x'][self.__P['x'] > 1.0] = 1.0
+		"""
+		arcadeSpots3 - arcadePopulation - primaryInfection()
+		:return:
+
+		Updates the exposition depending on the linear relationship between
+		the concentration of primary inoculum in the soil.
+		"""
+		randomValues = (np.random.rand(self.__sx*self.__sy, len(self.__patho)) < (self.__w/10000))
+		primaryInfections = (self.__P['A'] * randomValues.T).T
+		self.__x         += primaryInfections
+		self.__x[self.__x > 1.0] = 1.0
 
 	def nextCrop(self):
-		self.__P['x'][:] = 0.0
-		self.__P['y'][:] = False
+		"""
+		arcadeSpots3 - arcadePopulation - nextCrop()
+		:return:
+
+		Resets all the values but the primary inoculum
+		"""
+
+		self.__x[:,:] = 0.0
+		self.__y[:,:] = False
+		self.__d[:,:] = 0
 		self.__P['A'][:] = True
-		self.__P['d'][:] = 0
-
-
-
-	def plotPopulation(self):
+	def plotPopulation(self, patho, time=None, show=True):
 		import matplotlib.pyplot as plt
 		import seaborn as sns
-		exposition = self.__P['x'].reshape((self.__sx, self.__sy))
-		infective  = self.__P['y'].reshape((self.__sx, self.__sy))
+		try:
+			pathoLoc = self.__loc[patho]
+		except KeyError:
+			raise KeyError("There is no such a {0} pathotype".format(patho))
+		exposition = self.__x[:,pathoLoc].reshape((self.__sx, self.__sy))
+		infective  = self.__y[:,pathoLoc].reshape((self.__sx, self.__sy))
 		alive      = self.__P['A'].reshape((self.__sx, self.__sy))
-		primaryInoc= self.__P['w'].reshape((self.__sx, self.__sy))
+		primaryInoc= self.__w[:,pathoLoc].reshape((self.__sx, self.__sy))
 		#plt.figure(figsize=(10,5))
 		plt.subplot(221)
 		sns.heatmap(exposition, cmap='viridis', xticklabels=False, yticklabels=False)
@@ -116,21 +246,39 @@ class arcadePopulation:
 		plt.subplot(224)
 		sns.heatmap(primaryInoc, cmap='viridis', xticklabels=False, yticklabels=False)
 		plt.title('primary Inoculum')
+		if show:
+			plt.show()
+		else:
+			plt.savefig("patho_{0}_day_{1}.png".format(patho, time), dpi=300)
+			plt.close()
+	def plotInfectiveAreas(self, patho1, patho2):
+		import matplotlib.pyplot as plt
+		infectiveAreas = np.zeros((self.__sx, self.__sy,3))
+		patho1_loc = self.__loc[patho1]
+		patho2_loc = self.__loc[patho2]
+		infectiveAreas[:, :, 0] = self.__y[:, patho1_loc].reshape((self.__sx, self.__sy))
+		infectiveAreas[:, :, 1] = self.__y[:, patho2_loc].reshape((self.__sx, self.__sy))
+		plt.imshow(infectiveAreas)
 		plt.show()
 
-	def getStatistics(self):
+	def getStatistics(self, patho):
 		statistics = np.zeros(1, dtype=[
 			('exposition', 'float32'),
 			('infective',  'float32'),
 			('alive',      'float32'),
-			('inoculum',  'float32')
+			('inoculum',   'float32')
 		])
 		total_pop = self.__sy * self.__sx
-		statistics['exposition'] = np.sum(self.__P['x']) / total_pop
-		statistics['infective'] = np.sum(self.__P['y']) / total_pop
-		statistics['alive'] = np.sum(self.__P['A']) / total_pop
-		statistics['inoculum'] = np.sum(self.__P['w']) / (total_pop*1000)
+		try:
+			pathoLoc = self.__loc[patho]
+		except KeyError:
+			raise KeyError("There is no such a {0} pathotype".format(patho))
+		statistics['exposition'] = np.sum(self.__x[:,pathoLoc]) / total_pop
+		statistics['infective']  = np.sum(self.__y[:,pathoLoc]) / total_pop
+		statistics['alive']      = np.sum(self.__P['A']) / total_pop
+		statistics['inoculum']   = np.sum(self.__w[:,pathoLoc]) / (total_pop*1000)
 		return statistics
+
 
 
 
--- arcadeSimulator.py
+++ arcadeSimulator.py
@@ -9,24 +9,59 @@ import sys
 # NOTE : This is an script. Not the real object that will be used later
 sns.set_style('whitegrid')
 sns.set_palette('Set2')
-np.random.seed(54)
+np.random.seed(22)
 
 # initialization
 
 size_x = int(sys.argv[1])
 size_y = int(sys.argv[2])
 time   = int(sys.argv[3])
-randomSeed = np.random.randint(0, high=size_x*size_y, size=1)
+randomSeed = np.random.randint(0, high=size_x*size_y, size=2)
 
-population = aP.arcadePopulation(size_x, size_y)
-population.setSeed(randomSeed)
+#parametersDict = dict(pathotypes  = 'P0|P12', transmision = dict(P0 = 1.0, P12 = 0.75))
+parametersDict = dict(
+	pathotypes = ['P0', 'P12'],
+	beta = dict(
+		P0 = 1.0,
+		P12 = 0.75
+	),
+	n = dict(
+		P0 = 15.0,
+		P12 = 19.0
+	),
+	P = dict(
+		P0 = 1000,
+		P12 = 800
+	),
+	alpha = dict(
+		P0 = 1.0 - 3.50e-2,
+		P12 = 1.0 - 1.25e-2
+	),
+	C = np.array([
+		[1.0, -0.45],
+		[-0.19, 0.20]
+	])
+)
 
-statistics = np.empty(0, dtype=[
+population = aP.arcadePopulation(size_x, size_y, parametersDict)
+population.setSeed(randomSeed[0], patho='P0')
+population.setSeed(randomSeed[1], patho='P12')
+
+
+statistics = dict(
+	P0 = np.empty(0, dtype=[
+			('exposition', 'float32'),
+			('infective',  'float32'),
+			('alive',      'float32'),
+			('inoculum',  'float32')
+		]),
+	P12 = np.empty(0, dtype=[
 			('exposition', 'float32'),
 			('infective',  'float32'),
 			('alive',      'float32'),
 			('inoculum',  'float32')
 		])
+)
 
 # building interaction matrix
 
@@ -46,7 +81,7 @@ for ix in np.arange(size_x):
 
 		xx, yy = np.meshgrid(x_coordinates, y_coordinates)
 		dist = np.sqrt((xx ** 2) + (yy ** 2))
-		dist = np.exp(-2.50*dist)
+		dist = np.exp(-1.50*dist)
 		dist[dist < 1e-2] = 0.0
 
 		I[iZ,:] = dist.T.reshape((size_x * size_y))
@@ -55,23 +90,26 @@ for ix in np.arange(size_x):
 
 
 # simulation body
-for crop in range(15):
+for crop in range(1):
 	for i in range(time):
-		print("day %d" % (i))
+		print("day %d" % i)
 		population.updateAlive()
-		population.updateExposition(I.dot(population.getExposition()))
 		population.primaryInfection()
+		population.updateExposition(I.dot(population.getTranmission()))
 		population.updateInfective()
 		population.updateInoculum()
-		statistics = np.concatenate((statistics,population.getStatistics()))
+		statistics['P0'] = np.concatenate((statistics['P0'],population.getStatistics('P0')))
+		statistics['P12'] = np.concatenate((statistics['P12'],population.getStatistics('P12')))
+		#if i == 156:
+		#	population.plotPopulation('P12')
+
 	for i in range(365 - time):
 		population.updateInoculum()
 	population.nextCrop()
 
-plt.plot(statistics['exposition'], label="exposition")
-plt.plot(statistics['infective'], label="infective")
-plt.plot(statistics['alive'], label="alive")
-plt.plot(statistics['inoculum'], label="inoculum")
+plt.plot(statistics['P0']['infective'], label="P0")
+plt.plot(statistics['P12']['infective'], label="P12")
+
 plt.xlabel('Days')
 plt.ylabel('Value')
 plt.legend(loc=0)
